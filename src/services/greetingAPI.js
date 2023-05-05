422724e3be3953ea3abd6514c4cbc9124c8c0cc6
mule <belmeetmule@gmail.com>
2023-05-06 00:36:59 +0300

    feature: implement fetch greeting api

---
+++ src/services/greetingAPI.js
@@ -0,0 +1,9 @@
+// A mock function to mimic making an async request for data
+const baseURL = "http://127.0.0.1:3000/api/v1/greetings";
+
+
+ export const fetchGreeting = () => {
+    const response = fetch(baseURL)
+                    .then((response) => response.json());
+    return response;
+  }
