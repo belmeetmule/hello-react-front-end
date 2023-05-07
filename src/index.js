66caa089852879c62682a52277516baf8a569196
mule <belmeetmule@gmail.com>
2023-05-07 07:14:47 +0300

    Wrap provider with router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
@@ -10,11 +11,12 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </Router>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
