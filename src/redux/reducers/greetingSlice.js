7377187082631975304c6d25fda97fd880063ae9
mule <belmeetmule@gmail.com>
2023-05-07 07:12:38 +0300

    remove unnessary actions

--- src/redux/reducers/greetingSlice.js
+++ src/redux/reducers/greetingSlice.js
@@ -19,15 +19,7 @@ export const fetchAsync = createAsyncThunk(
     name: 'greeting',
     initialState,
     // The `reducers` field lets us define reducers and generate associated actions
-    reducers: {
-      load: (state, action) => {
-        // Redux Toolkit allows us to write "mutating" logic in reducers. It
-        // doesn't actually mutate the state because it uses the Immer library,
-        // which detects changes to a "draft state" and produces a brand new
-        // immutable state based off those changes
-        state.message = action.payload;
-      },
-    },
+    reducers: {},
 
     extraReducers: (builder) => {
         builder
